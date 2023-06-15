805606fc460ef3be34f9f52228a3430060b891a1
Dyako <Dyako@LAPTOP-USAVD0N8>
2023-06-15 16:00:03 +0600

    Добавьте файлы проекта.

---
+++ Palindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите фразу для проверки:");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine($"Введенная строка является палиндромом: {isPalindrome}");
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right)
+        {
+            if (!Char.IsLetter(input[left]))
+            {
+                left++;
+                continue;
+            }
+
+            if (!Char.IsLetter(input[right]))
+            {
+                right--;
+                continue;
+            }
+
+            if (Char.ToLower(input[left]) != Char.ToLower(input[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
